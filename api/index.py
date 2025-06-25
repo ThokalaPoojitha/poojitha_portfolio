fc35ea0daa4363ee84173a37fff8b13ed6b878c3
ThokalaPoojitha <150243959+ThokalaPoojitha@users.noreply.github.com>
2025-06-25 13:37:15 +0100

    updated index

--- api/index.py
+++ api/index.py
@@ -5,8 +5,10 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
 def home():
+    print("hello")
     return render_template('index.html')
 
 # Required for Vercel serverless handler
 def handler(environ, start_response):
+    print("hey")
     return app.wsgi_app(environ, start_response)
